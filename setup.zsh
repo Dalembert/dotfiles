4985fe3df3aafbf1c05962e5553e4c89ff868fc4
Jean-Baptiste le Ronde d'Alembert <dalembert@pm.me>
2020-02-11 06:11:30 +0100

    Create vim pack directory if it does not exsit

--- setup.zsh
+++ setup.zsh
@@ -1,5 +1,10 @@
 #!/bin/zsh
-# Setup dotfiles (as symlink) in user's home directory
+# Setup dotfiles (as symlink) in user's home directory.
+# Create plugin directory for vim
+
+function skipping_msg {
+    echo $1 already exists, skipping
+}
 
 # glob with `*` does expand to all files in directory
 # wrap in `(` and `)` to convert to array
@@ -15,10 +20,17 @@ FILES=(*[^setup.zsh^README.md])
 for f in "${FILES[@]}"
 do
     if [[ -a "$HOME/.$f" ]]; then
-        print Skipping $f
+        skipping_msg $f
     else
         print "$f: Create symbolic link from $HOME/.$f to $(pwd)/$f"
         ln -s "$(pwd)/$f" "$HOME/.$f"
     fi
 done
 
+# create vim pack directory
+VIM_START="$HOME/.vim/pack/$USER/start"
+if [[ -a $VIM_START ]]; then
+    skipping_msg $VIM_START
+else
+    git clone https://github.com/cormacrelf/vim-colors-github $VIM_START
+fi
