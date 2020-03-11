fe05ab8a8b1ff8b62e512b050af6734b6be3ba42
Dalembert <dalembert@pm.me>
2020-03-11 08:16:33 +0100

    Load all ssh keys beginning with 'id'

--- zsh/ssh.zsh
+++ zsh/ssh.zsh
@@ -1,6 +1,4 @@
-# TODO trim white space from NMBR_KEY
-# TODO ssh-add -l shows one line if no key is added, fix
-NMBR_KEY=$(ssh-add -l | wc -l)
-if [[ $NMBR_KEY == 0 ]]; then
-    ssh-add ~/.ssh/*
+ssh-add -l > /dev/null 2>$1
+if [[ $? != 0 ]]; then
+    ssh-add ~/.ssh/id*[^.pub]
 fi
