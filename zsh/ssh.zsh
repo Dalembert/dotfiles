17a9a8d887bcab90c417bfb0d1a2612af28e43cc
Dalembert <Dalembert@pm.me>
2020-03-26 15:56:00 +0100

    Check if ssh-keys exist before adding them

--- zsh/ssh.zsh
+++ zsh/ssh.zsh
@@ -1,5 +1,11 @@
+# Check if ssh agent has added ssh keys
 ssh-add -l > /dev/null 2>&1
 RET=$?
 if [[ $RET != 0 ]]; then
-    ssh-add ~/.ssh/id*[^.pub]
+    # Check if there exist ssh keys (files have to start with 'id')
+    RET=`/home/dalembert/.ssh/id*[^.pub] > /dev/null 2>&1`
+    if [[ $RET == 0 ]]
+    then
+        ssh-add ~/.ssh/id*[^.pub]
+    fi
 fi
