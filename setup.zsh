d2916228ba677ad956f4ae70d49941799c11b1c2
Dalembert <dalembert@pm.me>
2020-04-28 17:32:43 +0200

    Resolved two merge conflicts from previouse rebase

--- setup.zsh
+++ setup.zsh
@@ -8,11 +8,7 @@ function skipping_msg {
 
 # glob with `*` does expand to all files in directory
 # wrap in `(` and `)` to convert to array
-<<<<<<< HEAD
 FILES=(*[^setup.zsh\|README.md])
-=======
-FILES=(*[^setup.zsh^README.md])
->>>>>>> 81ab246... Exclude README file from getting symlinked
 
 # array[@] return all items as seperate word,
 # array[*] would return all items as one word
